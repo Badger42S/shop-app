bc036b15b44bc21adc87b542cf4dc940eb01c5b0
Badger42S <57646489+Badger42S@users.noreply.github.com>
2020-08-30 09:27:32 -0400

    diveded app

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Recipe} from '../recipe.modal';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes:Recipe[]=[
+    new Recipe('chopa', 'sharp', 'none')
+  ];
 
   constructor() { }
 
