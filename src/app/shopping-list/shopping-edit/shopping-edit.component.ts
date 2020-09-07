78979cfc58779f9a3bc5592acfce70173f86a19d
Badger42S <57646489+Badger42S@users.noreply.github.com>
2020-09-07 18:07:45 -0400

    add editing ingredient mode

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,20 +1,41 @@
-import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f', {static:false}) slform:NgForm;
 
+  subscription:Subscription;
+  editMode=false;
+  editedNumber:number;
+  editedItem:Ingredient;
+  
   constructor(private slService:ShoppingListService) { }
-
+  
   ngOnInit(): void {
+    this.subscription=this.slService.startedEditing
+    .subscribe((index:number)=>{
+      this.editedNumber=index;
+      this.editMode=true;
+      this.editedItem=this.slService.getIngredient(index);
+      this.slform.setValue({
+        name:this.editedItem.name,
+        amount:this.editedItem.amount
+      });
+    });
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+  
   onAdd(form:NgForm){
     const value=form.value;
     const newIngredient=new Ingredient(value.name, value.amount);
