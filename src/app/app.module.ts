e91fb9a7117724a5a87b9f1111e594e6beabd5b3
Badger42S <57646489+Badger42S@users.noreply.github.com>
2020-09-09 14:42:12 -0400

    delete ingredients from recipe, little bugs fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 import {ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeService } from './recipes/recipe.service';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [ShoppingListService],
+  providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,27 +1,33 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
 
 import {Recipe} from '../recipe.modal';
 import {RecipeService} from '../recipe.service';
 import {  Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes:Recipe[]=[];
+  recipeSubscr:Subscription;
 
   constructor(private recipeService: RecipeService, private router:Router, private route:ActivatedRoute) { }
-
+  
   ngOnInit() {
-    this.recipeService.recipeChanged
-      .subscribe(
-        (newRecipes:Recipe[])=>{
-          this.recipes=newRecipes;
-        }
+    this.recipeSubscr=this.recipeService.recipeChanged
+    .subscribe(
+      (newRecipes:Recipe[])=>{
+        this.recipes=newRecipes;
+      }
       )
-    this.recipes=this.recipeService.getRecipes();
+      this.recipes=this.recipeService.getRecipes();
+  }
+
+  ngOnDestroy(): void {
+    this.recipeSubscr.unsubscribe();
   }
   onNewRecipe(){
     this.router.navigate(['new'],{relativeTo:this.route});
