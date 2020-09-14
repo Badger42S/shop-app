20c8c7f213746aca1567866d1924f71c40af9dd4
Badger42S <57646489+Badger42S@users.noreply.github.com>
2020-09-13 23:14:53 -0400

    modify auth page

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  isLogin=true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSwitchMode(){
+    this.isLogin=!this.isLogin;
+  }
 }
